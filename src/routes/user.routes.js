9167d31a31b9e6220cad89110815d539b422faf0
Akansha <akanshapandey882004@gmail.com>
2024-12-16 15:25:45 +0530

    added routes for sub pipelines

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,8 +1,9 @@
 // src/routes/user.routes.js
 import { Router } from "express";
-import { registerUser,logOutUser,loginUser ,refreshAccessToken} from "../controllers/user.controller.js"; // Ensure correct path and export
+import { registerUser,logOutUser,loginUser ,refreshAccessToken, changeCurrentPassword, getUserChannelProfile, getUserWatchHistory} from "../controllers/user.controller.js"; // Ensure correct path and export
 import {upload} from "../middlewares/multer.middleware.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { verify } from "jsonwebtoken";
 const router = Router();
 console.log("problem");
 router.route("/register").post(
@@ -22,4 +23,13 @@ router.route("/login").post(loginUser);
 //secured routes
 router.route("/logout").post(verifyJWT,logOutUser);
 router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyJWT,changeCurrentPassword
+);
+router.route("/current-user").get(verifyJWT,getCurrentUser);
+router.route("/update-account-details").patch(verifyJWT,updateAccountDetails);
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar);
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage);
+router.route("/channel/:username").get(verifyJWT,getUserChannelProfile);
+router.route("/watchHistory").get(verifyJWT,getUserWatchHistory);
+
 export default router;
